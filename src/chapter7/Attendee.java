9d007a1532b35fe21ef25cd688f8b92ed1b498fb
안병휘 <haist2002@gmail.com>
2011-11-29 19:47:35 +0900

    챕터7 진행중

--- src/chapter7/Attendee.java
+++ src/chapter7/Attendee.java
@@ -1,5 +1,19 @@
 package chapter7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Attendee {
+
+	private List<String> attendeeList = new ArrayList<String>();
+	
+	public void add(String name) {
+		this.attendeeList.add(name);
+	}
+
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- test/chapter7/AttendeeTest.java
+++ test/chapter7/AttendeeTest.java
@@ -2,21 +2,30 @@ package chapter7;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AttendeeTest {
 
 	@Test
-	public void testAdd() {
+	public void testAdd() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
 		Attendee attendee = new Attendee();
 		attendee.add("æ»∫¥»÷");
-		assertEquals("æ»∫¥»÷", attendee.get(1));
+		
+		Field attendeeList = attendee.getClass().getDeclaredField("attendeeList");
+		
+		attendeeList.setAccessible(true);
+		assertEquals("æ»∫¥»÷",((List<String>)attendeeList.get(attendee)).get(0));
 	}
 	
-	@Test
+	@Ignore @Test
 	public void testGet(){
 		Attendee attendee = new Attendee();
 		attendee.add("æ»∫¥»÷");
 		assertEquals("æ»∫¥»÷", attendee.get(1));
+
 	}
 }
